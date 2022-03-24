68c4684ae35b358727465584aed379a34cb2795b
lillaro <aleksanderandrislr@gmail.com>
2022-03-24 09:10:55 +0100

    first experiments, it seems easy to catch the keys that the user sends and to not do that when you dont need to

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "better-keys" is now active!');
+	let activated: boolean = false;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('better-keys.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from better-keys!');
-	});
+	const keyPressed = vscode.commands.registerCommand(
+		"type",
+		(e: { text: string }) => {
+			console.log(e.text, activated);
+			console.log(e);
 
-	context.subscriptions.push(disposable);
+			if (!activated) {
+				vscode.commands.executeCommand("default:type", e);
+			}
+		}
+	);
+
+	const activateBKmode = vscode.commands.registerCommand(
+		"better-keys.activate",
+		(_) => {
+			activated = true;
+		}
+	);
+
+	const deactivateBKmode = vscode.commands.registerCommand(
+		"better-keys.deactivate",
+		(_) => {
+			activated = false;
+		}
+	);
+
+	context.subscriptions.push(activateBKmode);
+	context.subscriptions.push(deactivateBKmode);
+	context.subscriptions.push(keyPressed);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
